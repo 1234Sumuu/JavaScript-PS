c3e1af557e7145bbae765d9db58346a2e908910c
1234Sumuu <sumaiya.akter.cse@uab.edu.bd>
2021-07-08 22:53:42 +0600

    problem solving

---
+++ max.js
@@ -0,0 +1,6 @@
+var business = 850;
+var minister = 650;
+var sochib = 750;
+
+var max = Math.max(business, minister, sochib);
+console.log(max);
---
+++ random.js
@@ -0,0 +1,9 @@
+var num = 2.12458;
+var result = Math.floor(num);
+var result2 = Math.ceil(num);
+var result3 = Math.round(num);
+for(var i =0; i<10; i++){
+    var dice = Math.random() * 6;
+    var output = Math.round(dice);
+    console.log(output); 
+}
